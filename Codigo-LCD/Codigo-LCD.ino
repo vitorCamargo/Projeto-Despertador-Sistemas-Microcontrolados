5007778d86d8cf281fdfa2463de1af05355191b3
Lucas Vinicius ribeiro <lucasvribeiro14@gmail.com>
2018-11-16 23:24:04 -0200

    Código Exemplo LCD

---
+++ Codigo-LCD/Codigo-LCD.ino
@@ -0,0 +1,38 @@
+//Carrega a biblioteca LiquidCrystal
+#include <LiquidCrystal.h>
+ 
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 28, 29, 30, 31);
+ 
+void setup()
+{
+  //Define o número de colunas e linhas do LCD
+  lcd.begin(16, 2);
+}
+ 
+void loop()
+{
+  //Limpa a tela
+  lcd.clear();
+  //Posiciona o cursor na coluna 3, linha 0;
+  lcd.setCursor(3, 0);
+  //Envia o texto entre aspas para o LCD
+  lcd.print("DISCORDO");
+  lcd.setCursor(1, 1);
+  lcd.print("==============");
+  delay(5000);
+   
+//  //Rolagem para a esquerda
+//  for (int posicao = 0; posicao < 3; posicao++)
+//  {
+//    lcd.scrollDisplayLeft();
+//    delay(300);
+//  }
+//   
+//  //Rolagem para a direita
+//  for (int posicao = 0; posicao < 6; posicao++)
+//  {
+//    lcd.scrollDisplayRight();
+//    delay(300);
+//  }
+}
