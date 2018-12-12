f71a7930ec925c075bc2bbacd78d495b89c6abba
vitorCamargo <vitorcamargo@alunos.utfpr.edu.br>
2018-12-12 11:44:20 -0200

    Despertador Integrado com LCD/Buzzer/Botões

--- Despertador/Despertador.ino
+++ Despertador/Despertador.ino
@@ -2,6 +2,7 @@
 #include <EEPROM.h>
 #include <Keypad.h>
 #include <DS1307.h>
+#include <stdlib.h>
 
 #define TAM_HORA 5
 
@@ -14,6 +15,13 @@ unsigned long int intervalo = 0;
 byte value;
 char comando;
 String horaArmazenada, horaAtual;
+
+String StrConta;
+String StrResult = "    ";
+int Resultado = 0;
+String ResultIncorreto = "ERRRROOOOUUU!";
+String BomDia = "Bom dia!";
+int Acertos = 0;
 
 const byte linhas = 4, colunas = 4;
 char matrizteclado[linhas][colunas] = {
@@ -31,6 +39,24 @@ int buzzer = 32;
 LiquidCrystal lcd(12, 11, 28, 29, 30, 31);
 Keypad teclado = Keypad(makeKeymap(matrizteclado), pinoslinhas, pinoscolunas, linhas, colunas);
 DS1307 rtc(A4, A5);
+
+void geraConta() {
+    int opA = (int) random(1, 100);
+    int opB = (int) random(1, 100);
+    int op = (int) random(1, 3);
+    char strOp;
+    
+    if (op == 1) {
+        strOp = '*';
+        Resultado = opA * opB;
+    } else {
+        strOp = '+';
+        Resultado = opA + opB;
+    }
+
+    StrConta = (String(opA) + strOp + String(opB) + '=');
+    Serial.print(StrConta);
+}
 
 void limpa_eeprom() {
   for (int i = 0; i < EEPROM.length(); i++) { EEPROM.write(i, 0); }
@@ -54,14 +80,57 @@ void bluetooth_eeprom() {
 }
 
 void despertar() {
-  Serial.println("ACORDA MENINO");
+  tone(buzzer, 150);
+  geraConta();
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print(StrConta);
+
+  char apertatecla = '0';
+  int posTela = 0;
+  int resultUsuario = 0;
+  StrResult = "    ";
+  while (apertatecla != '*' && apertatecla != '#' && apertatecla != 'A' && apertatecla != 'B' && apertatecla != 'B' && apertatecla != 'C' && apertatecla != 'D') {
+      apertatecla = teclado.getKey();
+
+      if (apertatecla != '*' && apertatecla != '#' && apertatecla != 'A' && apertatecla != 'B' && apertatecla != 'B' && apertatecla != 'C' && apertatecla != 'D') {
+        if (apertatecla) {
+          lcd.setCursor(0, 1);
+          StrResult[posTela] = apertatecla;
+          lcd.print(StrResult);
+          resultUsuario = (resultUsuario * (10)) + atoi(&apertatecla);
+          posTela++;
+        }
+      }
+    }
+
+    Serial.println(resultUsuario);
+    
+    if (Resultado == resultUsuario) {
+      Acertos++;
+    } else {
+      lcd.clear();
+      lcd.setCursor(2, 0);
+      lcd.print(ResultIncorreto);
+      delay(5000);
+      despertar();
+    }
+
+    if (Acertos == 3) {
+      lcd.clear();
+      lcd.setCursor(2, 0);
+      lcd.print(BomDia);
+      noTone(buzzer);
+      delay(3000);
+      lcd.clear();
+      return ;
+    } else {
+      despertar();
+    }
 }
 
-void setup() {
+void setup () {
   rtc.halt(false);
-  rtc.setDOW(FRIDAY);
-  rtc.setTime(22, 05, 0);
-  rtc.setDate(30, 11, 2018);
   rtc.setSQWRate(SQW_RATE_1);
   rtc.enableSQW(true);
   
@@ -71,13 +140,18 @@ void setup() {
   lcd.begin(16, 2);
 }
  
-void loop() {
+void loop () {
    recebe_horas();
 
    if (Serial.available() != 0) { bluetooth_eeprom(); }
    else { addr = 0; }
 
-  if ((millis() - intervalo) >= 30000) {
+  if ((millis() - intervalo) >= 35000 || intervalo == 0) {
+    Serial.print("Hora Atual: ");
+    Serial.println(horaAtual);
+    Serial.print("Hora Despertador: ");
+    Serial.println(horaArmazenada);
+    
     if (horaArmazenada == "") {
       digitalWrite(led, LOW);
     } else {
@@ -88,6 +162,7 @@ void loop() {
       
       if (horaArmazenada == horaAtual) {
         despertar();
+        Acertos = 0;
       }
     }
     intervalo = millis();
